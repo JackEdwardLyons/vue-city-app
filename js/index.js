9358f0b3de80ccea7bd5c0f978d651203cad897d
Jack Lyons <jack.edward.lyons@gmail.com>
2017-05-21 23:29:21 -0600

    major refactor thanks to @jdtdesigns at FreeCodeCamp, thank you

--- js/index.js
+++ js/index.js
@@ -1,93 +1,96 @@
-var app = new Vue({
-  el: '.site-content',
+new Vue({
+  el: '#app',
   
   data: {
-    headline: `Search a City`,
-    citySearch: '',
-    cities: [],
-    relevantCities: [],
-    showMap: false,
-    searchFilters: false
-    // end data
+      input: '',
+      city: '',
+      cities: [],
+      results: [],
+      searchFilters: false,
+      show_modal: false
   },
+  
   methods: {
-    
-    addCity() {
-      this.defaultCity = '';
-      if (this.citySearch !== '' && this.citySearch.length >= 3) {
-        this.lookupCity();
-      } 
+    getCities() {
+      const url = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+            
+      axios.get(url)
+      .then(res => {
+        this.cities = res.data;
+      });
+      
     },
     
-    lookupCity: _.debounce(function() {
-      var cityName = this.citySearch;
-      // City data
-      const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
-      // Use axios library to make request
-      axios
-        .get(endpoint)
-        .then((response) => {
-          this.cities = response.data;
-        })
-        .catch(err => console.log(err));
-      // this.citySearch = '';
-    }, 400),
+    filterCities() {
+      this.results = [];
+      
+      if ( this.input.length > 2 ) {
+        
+        clearTimeout(window.delay);
+
+        window.delay = setTimeout(() => {
+          this.results = _.filter(this.cities, obj => {
+            return _.lowerCase(obj.city).includes(_.lowerCase(this.input));
+          });
+        }, 500);
+        
+      }
+    },
+    
+    showMap(city) {
+      this.city = city;
+      this.show_modal = true;
+    },
+    
+    closeModal() {
+      this.show_modal = false;
+    },
+    
     formattedNumber(x) {
       return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ', ');
     }
-        
-  }, // end methods
-  computed: {
-    filteredCities() {
-      let searched = this.citySearch;
-      
-      if (searched === '') this.cities = []; 
-      
-      return this.cities.filter((city) => {
-        return city.city.toLowerCase().indexOf( searched.toLowerCase() ) > -1 
-        // || city.state.toLowerCase().indexOf( searched.toLowerCase() ) > -1;
-      });
-    }
-    
-  } // end computed
+  },
+  
+  created() {
+    this.getCities();
+  }
 });
 
 
-
-var modal = Vue.component('modal', {
-  props: [ 'title' ],
-  template: `
-    <div class="modal is-active">
-      <div class="modal-background" @click="$emit('close')"></div>
-      <div class="modal-card">
-        <header class="modal-card-head">
-
-          <p class="modal-card-title">View {{ title }} on a map</p>
-          
-
-          <button class="delete" @click="$emit('close')"></button>
-        </header>
-        <section class="modal-card-body">
-
-          <iframe width="100%" height="350" frameborder="0" style="border:0" 
-                  :src="iframeUrl">
-          </iframe>
-
-        </section>
-        <footer class="modal-card-foot">
-
-          <a class="button is-success">Save changes</a>
-          <a class="button">Cancel</a>
-
-        </footer>
-      </div>
-    </div>
-  `,
-  computed: {
-    iframeUrl: function() {
-      console.log( this.title );
-      return `https://www.google.com/maps/embed/v1/place?q=${this.title},United+States&key=AIzaSyBh0g0ArtnfdANIyo-xH8v61n2bxrhMdME`;
+Vue.component('modal', {
+  template: '#map-modal',
+  
+  props: ['city'],
+  
+  data() {
+    return {
+      loaded: false,
+      map_url: ''
     }
+  },
+  
+  methods: {
+    showMap() {
+      const iframe = document.getElementById('iframe'),
+            url = `https://www.google.com/maps/embed/v1/place?q=${this.city},United+States&key=AIzaSyBh0g0ArtnfdANIyo-xH8v61n2bxrhMdME`,
+            // .onload will only work with es5 as of right now, so we need this
+            self = this;
+    
+      iframe.src = url;
+      console.log("Source: ", iframe.src);
+      console.log(self);
+      iframe.onload = function() {
+        self.loaded = true;
+      }
+    },
+    
+    closeModal() {
+      this.$emit('close_modal');
+    }
+  },
+  
+  mounted() {
+    this.showMap();
   }
 });
 
